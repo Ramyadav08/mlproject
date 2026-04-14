88dc34323fe036aa3f6f93a34004bc017ab4748f
ramrekha08 <ramrekhayadav412@gmail.com>
2026-04-14 09:23:42 +0545

    exception and logging completed

--- src/logger.py
+++ src/logger.py
@@ -14,5 +14,12 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
+console_handler=logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter=logging.Formatter("[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+logging.getLogger().addHandler(console_handler)
+
+
 if __name__=="__main__":
     logging.info("Logging has started")
