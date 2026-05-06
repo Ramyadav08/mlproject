7217616a36366237b1edb3894323490e2c18e693
ramrekha08 <ramrekhayadav412@gmail.com>
2026-05-06 21:54:02 +0530

    trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transforms import DataTransformation,DataTransformationConfig
+from src.components.model_train import ModelTrainer,ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig():
@@ -43,6 +44,9 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transform_obj=DataTransformation()
-    data_transform_obj.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transform_obj.initiate_data_transformation(train_data,test_data)
+    ModelTrainer_obj=ModelTrainer()
+    ModelTrainer_obj.initiate_model_trainer(train_arr,test_arr)
+    
     
     
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ from src.logger import logging
 from src.exception import CustomException
 import pandas as pd
 import numpy as np
+from sklearn.metrics import r2_score
 
 
 def save_object(file_path,obj):
@@ -15,4 +16,21 @@ def save_object(file_path,obj):
         logging.info("Object is pickled successfully")
     except Exception as e:
         logging.info("Error occured in saving object")
+        raise CustomException(e,sys)
+    
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report={}
+        for i in range(len(models)):
+            model=list(models.values())[i]
+            model.fit(X_train,y_train)
+            
+            y_test_pred=model.predict(X_test)
+            
+            report[list(models.keys())[i]]=r2_score(y_test,y_test_pred)
+            
+        return report
+    except Exception as e:
+        logging.info("Error occured in evaluating models")
         raise CustomException(e,sys)
